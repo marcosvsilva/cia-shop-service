49adce5192c38cb2efb672e7b6938231ce417fc0
Marcos Vinicius <marcos.v.silva@live.com>
2019-02-11 00:24:13 -0200

    Final appresetention application server

--- server/application/application.py
+++ server/application/application.py
@@ -1,7 +1,13 @@
 from CiaShopServer.server.controller.order_controller import OrderController
+from CiaShopServer.server.service.token import Token
 
 order_controller = OrderController()
 orders = order_controller.get_orders()
+token = Token()
 
 for order in orders:
-    print(order.id)
+    print('-------------------')
+    print('Print orders list in {}'.format(token.get_store_name()))
+    order.print_order()
+    print('-------------------')
+    print('\n')
--- server/model/order.py
+++ server/model/order.py
@@ -25,3 +25,21 @@ class Order(object):
             discounts_fees.append(DiscountsFess(discount_fee))
 
         return discounts_fees
+
+    def print_order(self):
+        print('Order id: {}'.format(self.id))
+        print('Order createdAt: {}'.format(self.createdAt))
+        print('Order updatedAt: {}'.format(self.updatedAt))
+        print('Order deliveredAt: {}'.format(self.deliveredAt))
+        print('Order deliveryEstimatedDate: {}'.format(self.deliveryEstimatedDate))
+        print('Order status: {}'.format(self.status))
+        print('Order cancellationReason: {}'.format(self.cancellationReason))
+        print('Order statusMsgStore: {}'.format(self.statusMsgStore))
+        print('Order statusMsgCustomer: {}'.format(self.statusMsgCustomer))
+        print('Order templateId: {}'.format(self.templateId))
+        print('Order subtotal: {}'.format(self.subtotal))
+        for discont_fee in self.discountsAndFees:
+            print('Discount type: {}'.format(discont_fee.type))
+            print('Discount value: {}'.format(discont_fee.value))
+            print('Discount description: {}'.format(discont_fee.description))
+            print('Discount source: {}'.format(discont_fee.source))

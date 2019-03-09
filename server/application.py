d30f3d7e64934a45e3d2ccfe6dbd1a1f3325b866
Marcos Vinicius <marcos.v.silva@live.com>
2019-03-09 11:59:53 -0300

    Create procedure register log

--- server/application.py
+++ server/application.py
@@ -1,3 +1,4 @@
+import datetime
 from server.controller.product_controller import ProductController
 from server.service.config import Config
 
@@ -6,11 +7,22 @@ class Application(object):
 
     def __init__(self):
         product_controller = ProductController()
-        self.products = product_controller.get_orders()
+        self.__config = Config()
+        self.__products = product_controller.get_orders()
 
     def syncronize(self):
-        for product in self.products:
+        self.generate_log('start process')
+        for product in self.__products:
+            self.generate_log('print product')
             print('-------------------')
             product.response.print_json()
             print('-------------------')
             print('\n')
+
+    def generate_log(self, log):
+        log_file = '{}\\{}.{}'.format(self.__config.log_path, self.__config.log_name, self.__config.log_extension)
+        with open(log_file, 'w') as file:
+            file.write('{}: {}'.format(datetime.datetime.now(), log))
+
+application = Application()
+application.syncronize()

c93cd07e2781f303228b90cb34c76cb5e8a91856
Marcos Vinicius <marcos.v.silva@live.com>
2019-03-24 15:23:56 -0300

    exception tratament

--- server/_request.py
+++ server/_request.py
@@ -1,8 +1,6 @@
 import requests
 import json
 from _config import Config, generate_log
-from _config import Token
-import pprint
 
 
 class Request:
@@ -25,20 +23,24 @@ class Request:
         self.request.headers.update(content_type)
             
     def get_list(self, table, max_id=0):
-        if max_id > 0:
-            url_request = '{}://{}/api/{}/{}/?minId={}'.format(self.protocol, self.store_name, self.version_api, table,
-                                                               max_id)
-        else:
-            url_request = '{}://{}/api/{}/{}'.format(self.protocol, self.store_name, self.version_api, table)
+        try:
+            if max_id > 0:
+                url_request = '{}://{}/api/{}/{}/?minId={}'.format(self.protocol, self.store_name, self.version_api, table,
+                                                                   max_id)
+            else:
+                url_request = '{}://{}/api/{}/{}'.format(self.protocol, self.store_name, self.version_api, table)
 
-        config = Config()
-        if config.system_print_url_request_log:
-            generate_log('url api request: {}'.format(url_request))
+            config = Config()
+            if config.system_print_url_request_log:
+                generate_log('url api request: {}'.format(url_request))
 
-        request = self.request.get(url_request)
-        json_request = json.loads(request.text)
+            request = self.request.get(url_request)
+            json_request = json.loads(request.text)
 
-        if len(json_request) == config.api_register_max_returns:
-            json_request + self.get_list(table, json_request[config.api_register_max_returns - 1]['id'])
+            if len(json_request) == config.system_register_max_returns:
+                json_request + self.get_list(table, json_request[config.system_register_max_returns - 1]['id'])
 
-        return json_request
+            return json_request
+        except Exception as fail:
+            generate_log('exception api request, fail: {}'.format(fail))
+            return None
